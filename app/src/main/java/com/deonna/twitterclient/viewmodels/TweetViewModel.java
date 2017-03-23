803c81d9410e1d15a652860d90eab2dee83f6574
Deonna Hodges <dnhodges@gmail.com>
2017-03-23 11:43:16 -0700

    feat(timeline): Hooked up tweet properties to view.

--- app/src/main/java/com/deonna/twitterclient/viewmodels/TweetViewModel.java
+++ app/src/main/java/com/deonna/twitterclient/viewmodels/TweetViewModel.java
@@ -1,6 +1,7 @@
 package com.deonna.twitterclient.viewmodels;
 
 import android.content.Context;
+import android.view.View;
 
 import com.deonna.twitterclient.models.Tweet;
 
@@ -15,8 +16,38 @@ public class TweetViewModel {
         this.tweet = tweet;
     }
 
+    public String getName() {
+
+        return tweet.user.name;
+    }
+
+    public int getIsVerifiedVisibility() {
+
+        if (tweet.user.isVerified) {
+
+            return View.VISIBLE;
+        }
+
+        return View.GONE;
+    }
+
+    public String getScreenName() {
+
+        return String.format("@%s", tweet.user.screenName);
+    }
+
     public String getTweetText() {
 
         return tweet.text;
+    }
+
+    public String getRetweetCount() {
+
+        return tweet.retweetCount;
+    }
+
+    public String getFavoriteCount() {
+
+        return tweet.favoriteCount;
     }
 }
