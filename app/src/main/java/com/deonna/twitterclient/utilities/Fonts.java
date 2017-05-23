b1ae02e17ffba9caf6feef4041fa4bb968af4f54
Deonna Hodges <dnhodges@gmail.com>
2017-05-23 08:35:56 -0700

    refactor(fonts): Don't need to reference the names anywhere else. Using strings directly.

--- app/src/main/java/com/deonna/twitterclient/utilities/Fonts.java
+++ app/src/main/java/com/deonna/twitterclient/utilities/Fonts.java
@@ -11,18 +11,12 @@ public class Fonts {
     public static Typeface fontRegular;
     public static Typeface fontThin;
 
-    private static String fontBoldName = "Lato-Bold.ttf";
-    private static String fontExtraBoldName = "Lato-Black.ttf";
-    private static String fontLightName = "Lato-Light.ttf";
-    private static String fontRegularName = "Lato-Regular.ttf";
-    private static String fontThinName = "Lato-Thin.ttf";
-
     public static void setupFonts(AssetManager assetManager) {
 
-        fontBold = Typeface.createFromAsset(assetManager, fontBoldName);
-        fontExtraBold = Typeface.createFromAsset(assetManager, fontExtraBoldName);
-        fontLight = Typeface.createFromAsset(assetManager, fontLightName);
-        fontRegular = Typeface.createFromAsset(assetManager, fontRegularName);
-        fontThin = Typeface.createFromAsset(assetManager, fontThinName);
+        fontBold = Typeface.createFromAsset(assetManager, "Lato-Bold.ttf");
+        fontExtraBold = Typeface.createFromAsset(assetManager, "Lato-Black.ttf");
+        fontLight = Typeface.createFromAsset(assetManager, "Lato-Light.ttf");
+        fontRegular = Typeface.createFromAsset(assetManager, "Lato-Regular.ttf");
+        fontThin = Typeface.createFromAsset(assetManager, "Lato-Thin.ttf");
     }
 }
